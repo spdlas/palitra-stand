ae9c7f4c2d396be0180031360415c008685459bc
spdlas <89684642129@mail.ru>
2025-02-27 16:50:26 +0300

    FirstCommit

---
+++ JS/backbutton.js
@@ -0,0 +1,7 @@
+const buttonBack = document.getElementById('buttonBack');
+
+buttonBack.addEventListener("click", function (e) {
+    history.back();
+})
+
+// <div class="button-back" id="buttonBack">НАЗАД</div>
