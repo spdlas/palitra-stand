418118fedd60b29aebc8c5af1a8e5b903c1c9359
spdlas <89684642129@mail.ru>
2025-06-03 15:26:40 +0300

    Добавлены кружки

---
+++ page/activ/data-active.js
@@ -0,0 +1,369 @@
+let actKuncevo = [
+    {
+        actName: 'Лучики',
+        actDesc:'',
+        prepod: 'Васильева Ирина Александровна',
+        photo: 'VasilivaIA.png',
+        phone: '8 (495) 416-84-35',
+        email: 'VasilevaIA2@social.mos.ru'
+    },
+    {
+        actName: 'Знайка',
+        actDesc:'',
+        prepod: 'Васильева Иринна Александровна',
+        photo: 'VasilivaIA.png',
+        phone: '8 (495) 416-84-35',
+        email: 'VasilevaIA2@social.mos.ru'
+    },
+    {
+        actName: 'Подростки XXI века',
+        actDesc:'Подростковый клуб',
+        prepod: 'Мещеряков Анатолий Викторович',
+        photo: 'MeshAV.png',
+        phone: '',
+        email: 'MescheryakovAV@social.mos.ru'
+    }
+]
+
+let actKrylo = [
+    {
+        actName: 'Творческая мастерсская',
+        actDesc:'',
+        prepod: 'Абашникова Ольга Борисовна',
+        photo: 'AbashnikovaOB.png',
+        phone: '8 (495) 413-99-55',
+        email: 'AbashnikovaOB@social.mos.ru'
+    },
+    {
+        actName: 'Без стрессов',
+        actDesc: 'Занятия для взрослых',
+        prepod: 'Сидорова Марина Владимировна',
+        photo: 'SidorovaMV.png',
+        phone: '8 (495) 413-99-55',
+        email: 'SidorovaMV4@social.mos.ru'
+    },
+    {
+        actName: 'Подростки XXI века',
+        actDesc: 'Подростковый клуб',
+        prepod: 'Сидорова Марина Владимировна',
+        photo: 'SidorovaMV.png',
+        phone: '8 (495) 413-99-55',
+        email: 'SidorovaMV4@social.mos.ru'
+    },
+]
+
+let actMoj = [
+    {
+        actName: 'Изомания',
+        actDesc: '',
+        prepod: 'Хурумова Эка Павловна',
+        photo: 'HurumovaEP.png',
+        phone: '8 (495) 447-00-74',
+        email: 'KhurumovaEP@social.mos.ru'
+    },
+    {
+        actName: 'Ритмика',
+        actDesc: '',
+        prepod: 'Хурумова Эка Павловна',
+        photo: 'HurumovaEP.png',
+        phone: '8 (495) 447-00-74',
+        email: 'KhurumovaEP@social.mos.ru'
+    },
+    {
+        actName: 'ХэндМэйдКлаб',
+        actDesc: '',
+        prepod: 'Хурумова Эка Павловна',
+        photo: 'HurumovaEP.png',
+        phone: '8 (495) 447-00-74',
+        email: 'KhurumovaEP@social.mos.ru'
+    },
+    {
+        actName: 'Подростки XXI века',
+        actDesc: 'Подростковый клуб',
+        prepod: 'Хурумова Эка Павловна',
+        photo: 'HurumovaEP.png',
+        phone: '8 (495) 447-00-74',
+        email: 'KhurumovaEP@social.mos.ru'
+    },
+]
+
+let actVnuk = [
+    {
+        actName: 'Дошколята',
+        actDesc: 'Занятия с детьми до шокльного возраста',
+        prepod: 'Безбокова Ольга Викторовна',
+        photo: 'bezbokovaov.png',
+        phone: '8 (495) 736-04-55',
+        email: 'BezbokovaOV@social.mos.ru'
+    },
+    {
+        actName: 'Логопед',
+        actDesc: 'Занятия с логопедом',
+        prepod: 'Безбокова Ольга Викторовна',
+        photo: 'bezbokovaov.png',
+        phone: '8 (495) 736-04-55',
+        email: 'BezbokovaOV@social.mos.ru'
+    },
+    {
+        actName: 'Мир хобби',
+        actDesc: '',
+        prepod: 'Лаздин Елена Владимировна',
+        photo: 'LazdinEV.png',
+        phone: '8 (495) 736-04-55',
+        email: 'LazdinEV@social.mos.ru'
+    },
+]
+
+let actSoln = [
+    {
+        actName: 'Арт Ажур',
+        actDesc: '',
+        prepod: 'Подгорная Наталья Теодоровна',
+        photo: 'PodgornayaNT.png',
+        phone: '8 (495) 738-02-22',
+        email: 'PodgornayaNT1@social.mos.ru'
+    }
+]
+
+let actTrop = [
+    {
+        actName: 'Кино и попкорн',
+        actDesc: '',
+        prepod: 'Брауэр Кристина Михайловна',
+        photo: 'BrauerKM.png',
+        phone: '8 (499) 792-05-56',
+        email: 'BrauerKM@social.mos.ru'
+    },
+    {
+        actName: 'Твой час медиации',
+        actDesc: '',
+        prepod: 'Иванин Виктор Сергеевич',
+        photo: 'IvaninVS.png',
+        phone: '8 (499) 792-05-56',
+        email: 'IvaninVS@social.mos.ru'
+    },
+    // {
+    //     actName: 'Беседа с психологом',
+    //     actDesc: '',
+    //     prepod: 'Азизова Ирина Антольевна',
+    //     photo: 'AzizovaIA.png',
+    //     phone: '8 (499) 792-05-56',
+    //     email: 'AzizovaIA@social.mos.ru'
+    // },
+    // {
+    //     actName: 'Учимся дружить',
+    //     actDesc: '',
+    //     prepod: 'Клокова Ольга Николаевна',
+    //     photo: 'KlokovaON.png',
+    //     phone: '8 (499) 792-05-56',
+    //     email: 'KlokovaON@social.mos.ru'
+    // },
+    // {
+    //     actName: 'Учимся общаться',
+    //     actDesc: '',
+    //     prepod: 'Мамедова Мирвари Исмаил Кызы',
+    //     photo: 'MamedovaMI.png',
+    //     phone: '8 (499) 792-05-56',
+    //     email: 'MamedovaMI1@social.mos.ru'
+    // },
+    {
+        actName: 'Карусель',
+        actDesc: '',
+        prepod: 'Клокова Ольга Николаевна',
+        photo: 'KlokovaON.png',
+        phone: '8 (499) 792-05-56',
+        email: 'KlokovaON@social.mos.ru'
+    },
+    {
+        actName: 'Работа с чувствами',
+        actDesc: '',
+        prepod: 'Брауэр Кристина Михайловна',
+        photo: 'BrauerKM.png',
+        phone: '8 (499) 792-05-56',
+        email: 'BrauerKM@social.mos.ru'
+    },
+    {
+        actName: 'Подростки XXI века',
+        actDesc: 'Подростковый клуб',
+        prepod: 'Туполева Юлия Дмитриевна',
+        photo: 'TupolevaUD.png',
+        phone: '8 (499) 792-05-56',
+        email: 'TupolevaYD@social.mos.ru'
+    },
+    {
+        actName: 'Мой друг взрослый',
+        actDesc: '',
+        prepod: 'Азизова Ирина Антольевна',
+        photo: 'AzizovaIA.png',
+        phone: '8 (499) 792-05-56',
+        email: 'AzizovaIA@social.mos.ru'
+    },
+    // {
+    //     actName: 'Родительский клуб',
+    //     actDesc: '',
+    //     prepod: 'Азизова Ирина Антольевна',
+    //     photo: 'AzizovaIA.png',
+    //     phone: '8 (499) 792-05-56',
+    //     email: 'AzizovaIA@social.mos.ru'
+    // },
+]
+
+let actKutuz =  [
+    {
+        actName: 'Подростки XXI века',
+        actDesc: '',
+        prepod: 'Канарейкина Наталья Алексеевна',
+        photo: 'KanareykinaNA.png',
+        phone: '8 (499) 243-60-34',
+        email: 'KanarejkinaNA@social.mos.ru'
+    },
+]
+
+let actRamenki = [
+    {
+        actName: 'Подростки XXI века',
+        actDesc: '',
+        prepod: 'Канарейкина Наталья Алексеевна',
+        photo: 'KanareykinaNA.png',
+        phone: '8 (495) 734-00-60',
+        email: 'KanarejkinaNA@social.mos.ru'
+    },
+    {
+        actName: 'Творческая мастерская "Радость"',
+        actDesc: '',
+        prepod: 'Тюренкова Елена Алексеевна',
+        photo: 'TurenkovaEA.png',
+        phone: '8 (495) 734-00-60',
+        email: 'TyurenkovaEA@social.mos.ru'
+    },
+]
+
+
+const slideTropar = document.getElementById('slideTropar');
+const slideKrylo = document.getElementById('slideKrylo');
+const slideKunc = document.getElementById('slideKunc');
+const slideMoj = document.getElementById('slideMoj');
+const slideSoln = document.getElementById('slideSoln');
+const slideVnuk = document.getElementById('slideVnuk');
+const slideRam = document.getElementById('slideRam');
+const slideKut = document.getElementById('slideKut');
+
+actTrop.map((item) => {
+    slideTropar.innerHTML += `
+    <div class="active-item">
+        <div class="active-photo"><img src="../branch/img/${item.photo}" alt=""></div>
+        <div class="active-info">
+           <div class="title">${item.actName}</div>
+           <div class="desc">${item.actDesc}</div>
+           <div class="prep">${item.prepod}</div>
+           <div class="phone">${item.phone}</div>
+           <div class="email">${item.email}</div>
+        </div>
+    </div>
+    `
+})
+
+actKrylo.map((item) => {
+    slideKrylo.innerHTML += `
+    <div class="active-item">
+        <div class="active-photo"><img src="../branch/img/${item.photo}" alt=""></div>
+        <div class="active-info">
+           <div class="title">${item.actName}</div>
+           <div class="desc">${item.actDesc}</div>
+           <div class="prep">${item.prepod}</div>
+           <div class="phone">${item.phone}</div>
+           <div class="email">${item.email}</div>
+        </div>
+    </div>
+    `
+})
+
+actKuncevo.map((item) => {
+    slideKunc.innerHTML += `
+    <div class="active-item">
+        <div class="active-photo"><img src="../branch/img/${item.photo}" alt=""></div>
+        <div class="active-info">
+           <div class="title">${item.actName}</div>
+           <div class="desc">${item.actDesc}</div>
+           <div class="prep">${item.prepod}</div>
+           <div class="phone">${item.phone}</div>
+           <div class="email">${item.email}</div>
+        </div>
+    </div>
+    `
+})
+
+actMoj.map((item) => {
+    slideMoj.innerHTML += `
+    <div class="active-item">
+        <div class="active-photo"><img src="../branch/img/${item.photo}" alt=""></div>
+        <div class="active-info">
+           <div class="title">${item.actName}</div>
+           <div class="desc">${item.actDesc}</div>
+           <div class="prep">${item.prepod}</div>
+           <div class="phone">${item.phone}</div>
+           <div class="email">${item.email}</div>
+        </div>
+    </div>
+    `
+})
+
+actSoln.map((item) => {
+    slideSoln.innerHTML += `
+    <div class="active-item">
+        <div class="active-photo"><img src="../branch/img/${item.photo}" alt=""></div>
+        <div class="active-info">
+           <div class="title">${item.actName}</div>
+           <div class="desc">${item.actDesc}</div>
+           <div class="prep">${item.prepod}</div>
+           <div class="phone">${item.phone}</div>
+           <div class="email">${item.email}</div>
+        </div>
+    </div>
+    `
+})
+
+actVnuk.map((item) => {
+    slideVnuk.innerHTML += `
+    <div class="active-item">
+        <div class="active-photo"><img src="../branch/img/${item.photo}" alt=""></div>
+        <div class="active-info">
+           <div class="title">${item.actName}</div>
+           <div class="desc">${item.actDesc}</div>
+           <div class="prep">${item.prepod}</div>
+           <div class="phone">${item.phone}</div>
+           <div class="email">${item.email}</div>
+        </div>
+    </div>
+    `
+})
+
+actRamenki.map((item) => {
+    slideRam.innerHTML += `
+    <div class="active-item">
+        <div class="active-photo"><img src="../branch/img/${item.photo}" alt=""></div>
+        <div class="active-info">
+           <div class="title">${item.actName}</div>
+           <div class="desc">${item.actDesc}</div>
+           <div class="prep">${item.prepod}</div>
+           <div class="phone">${item.phone}</div>
+           <div class="email">${item.email}</div>
+        </div>
+    </div>
+    `
+})
+
+actKutuz.map((item) => {
+    slideKut.innerHTML += `
+    <div class="active-item">
+        <div class="active-photo"><img src="../branch/img/${item.photo}" alt=""></div>
+        <div class="active-info">
+           <div class="title">${item.actName}</div>
+           <div class="desc">${item.actDesc}</div>
+           <div class="prep">${item.prepod}</div>
+           <div class="phone">${item.phone}</div>
+           <div class="email">${item.email}</div>
+        </div>
+    </div>
+    `
+})
